96030bcbe597f226dfae9af71b1691b37ee0e2c2
zaack1337 <isaacgameramos@gmail.com>
2025-06-13 21:23:07 -0300

    add a routes file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import express from "express"
+import {router} from "./routes/routes.js"
 const app = express()
+app.use(router)
 const port = 5000
+
+app.get("/secret", (req, res) => {res.send("tu leu o codigo fonte né seu gay")})
 
 app.listen(port, () => {
     console.log("server is running 🚀")
